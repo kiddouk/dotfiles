87eef45a8d145d5ef846c276cdbd3463cf7c9582
Sebastien Requiem <sebastien.requiem@gmail.com>
2016-10-06 11:36:24 +0200

    Add oh my zsh to th bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+./brew.sh
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Now copy the conf file
+rm ~/.zshrc
+ln -s `pwd`/zsh/.zshrc ~/.zshrc
+mkdir ~/.zsh
+ln -s `pwd`/zsh/completions ~/.zsh/
+
 cd "$(dirname "${BASH_SOURCE}")";
 
 for i in .*; do
